b246eb0419c9677e8b94bd6e0c5d875f93448559
jakiichu <zeLLda444@gmail.com>
2025-08-04 18:57:00 +0700

    fix npm run build

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {Provider} from 'react-redux'
-import { store } from './app/config/store/core/index.ts'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
